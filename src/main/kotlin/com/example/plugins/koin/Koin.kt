ee26ea5508ed6a691487034c5e6f94f4e16ea500
Chesire <troyrijkaard@hotmail.co.uk>
2022-03-06 21:31:44 +0000

    Add Koin plugin for Ktor

---
+++ src/main/kotlin/com/example/plugins/koin/Koin.kt
@@ -0,0 +1,16 @@
+package com.example.plugins.koin
+
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import org.koin.ktor.ext.Koin
+
+// import org.koin.ktor.ext.Koin
+
+/**
+ * Installs and configures the [Koin] plugin.
+ */
+fun Application.configureKoin() {
+    install(Koin) {
+        modules(defaultModules)
+    }
+}
